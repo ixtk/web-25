e804b53b27c5778a6290c519e2410a63a8ea1ef4
ixtk <ikhcamot@gmail.com>
2024-12-06 20:41:12 +0400

    Add profile field states

--- lesson-code/frontend/src/forms/App.jsx
+++ lesson-code/frontend/src/forms/App.jsx
@@ -1,6 +1,42 @@
 import "./App.css"
+import { useState } from "react"
 
 export const ProfileControlledInputs = () => {
+  const [name, setName] = useState("")
+  const [age, setAge] = useState(null)
+  const [url, setUrl] = useState(
+    "https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg"
+  )
+  const [about, setAbout] = useState("")
+
+  const updateName = (event) => {
+    // console.log(event)
+    setName(event.target.value)
+  }
+
+  const updateAge = (event) => {
+    setAge(event.target.value)
+  }
+
+  const updateAvatarUrl = (event) => {
+    setUrl(event.target.value)
+  }
+
+  const updateAboutText = (event) => {
+    setAbout(event.target.value)
+  }
+
+  const resetProfile = () => {
+    setName("")
+    setAge(null)
+    setUrl(
+      "https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg"
+    )
+    setAbout("")
+  }
+
+  // console.log("Running :)")
+
   return (
     <div className="container">
       <div>
@@ -8,31 +44,33 @@ export const ProfileControlledInputs = () => {
           <legend>Create Profile</legend>
           <div>
             <label htmlFor="name">Name</label>
-            <input type="text" />
+            <input type="text" onChange={updateName} required minLength={3} />
           </div>
           <div>
             <label htmlFor="age">Age</label>
-            <input type="number" />
+            <input type="number" onChange={updateAge} />
           </div>
           <div>
             <label htmlFor="avatarUrl">Avatar URL</label>
-            <input type="url" />
+            <input type="url" onChange={updateAvatarUrl} />
           </div>
           <div>
             <label htmlFor="about">About</label>
-            <textarea />
+            <textarea onChange={updateAboutText} />
           </div>
           <div className="button-container">
             <button>Save profile</button>
-            <button>Reset all</button>
+            <button onClick={resetProfile}>Reset all</button>
           </div>
         </fieldset>
       </div>
       <div className="user-card">
-        <img src="https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg" />
-        <h1></h1>
-        <p></p>
-        <p></p>
+        <img src={url} />
+        <h1>
+          {name} {age === null ? "" : `(${age})`}
+        </h1>
+        {/* {age !== null && <span>({age})</span>} */}
+        <p>{about}</p>
       </div>
     </div>
   )
