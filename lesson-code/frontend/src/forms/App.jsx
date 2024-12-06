7816ac241df58b1d45cd9387f904020c85125025
ixtk <ikhcamot@gmail.com>
2024-12-06 19:15:08 +0400

    Temp rollback

--- lesson-code/frontend/src/forms/App.jsx
+++ lesson-code/frontend/src/forms/App.jsx
@@ -1,40 +1,6 @@
 import "./App.css"
-import { useState } from "react"
 
 export const ProfileControlledInputs = () => {
-  const [name, setName] = useState("")
-  const [age, setAge] = useState(null)
-  const [about, setAbout] = useState("")
-  const [url, setUrl] = useState(
-    "https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg"
-  )
-
-  const updateAboutText = (event) => {
-    setAbout(event.target.value)
-  }
-
-  const updateName = (event) => {
-    // console.log(event)
-    setName(event.target.value)
-  }
-
-  const updateAge = (event) => {
-    setAge(event.target.value)
-  }
-
-  const updateAvatarUrl = (event) => {
-    setUrl(event.target.value)
-  }
-
-  const resetFields = () => {
-    setName("")
-    setAge(null)
-    setAbout("")
-    setUrl(
-      "https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg"
-    )
-  }
-
   return (
     <div className="container">
       <div>
@@ -42,31 +8,31 @@ export const ProfileControlledInputs = () => {
           <legend>Create Profile</legend>
           <div>
             <label htmlFor="name">Name</label>
-            <input type="text" onChange={updateName} />
+            <input type="text" />
           </div>
           <div>
             <label htmlFor="age">Age</label>
-            <input type="number" onChange={updateAge} />
+            <input type="number" />
           </div>
           <div>
             <label htmlFor="avatarUrl">Avatar URL</label>
-            <input type="url" onChange={updateAvatarUrl} />
+            <input type="url" />
           </div>
           <div>
             <label htmlFor="about">About</label>
-            <textarea onChange={updateAboutText} />
+            <textarea />
           </div>
           <div className="button-container">
             <button>Save profile</button>
-            <button onClick={resetFields}>Reset all</button>
+            <button>Reset all</button>
           </div>
         </fieldset>
       </div>
       <div className="user-card">
-        <img src={url} />
-        <h1>{name}</h1>
-        <p>{age}</p>
-        <p>{about}</p>
+        <img src="https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg" />
+        <h1></h1>
+        <p></p>
+        <p></p>
       </div>
     </div>
   )
