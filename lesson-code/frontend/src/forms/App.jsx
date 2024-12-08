3b04bd13861533bd310d73a5e83dde2e17d4cdb2
ixtk <ikhcamot@gmail.com>
2024-12-08 12:48:36 +0400

    Finish reset code

--- lesson-code/frontend/src/forms/App.jsx
+++ lesson-code/frontend/src/forms/App.jsx
@@ -3,19 +3,20 @@ import { useState } from "react"
 
 export const ProfileControlledInputs = () => {
   const [name, setName] = useState("")
-  const [age, setAge] = useState(null)
+  const [age, setAge] = useState("")
   const [url, setUrl] = useState(
     "https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg"
   )
   const [about, setAbout] = useState("")
 
   const updateName = (event) => {
-    // console.log(event)
+    console.log(event)
     setName(event.target.value)
   }
 
   const updateAge = (event) => {
-    setAge(event.target.value)
+    console.log(event.target.value, typeof event.target.value)
+    setAge(Number(event.target.value))
   }
 
   const updateAvatarUrl = (event) => {
@@ -28,7 +29,7 @@ export const ProfileControlledInputs = () => {
 
   const resetProfile = () => {
     setName("")
-    setAge(null)
+    setAge("")
     setUrl(
       "https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg"
     )
@@ -44,19 +45,25 @@ export const ProfileControlledInputs = () => {
           <legend>Create Profile</legend>
           <div>
             <label htmlFor="name">Name</label>
-            <input type="text" onChange={updateName} required minLength={3} />
+            <input
+              type="text"
+              value={name}
+              onChange={updateName}
+              required
+              minLength={3}
+            />
           </div>
           <div>
             <label htmlFor="age">Age</label>
-            <input type="number" onChange={updateAge} />
+            <input type="number" value={age} onChange={updateAge} />
           </div>
           <div>
             <label htmlFor="avatarUrl">Avatar URL</label>
-            <input type="url" onChange={updateAvatarUrl} />
+            <input type="url" value={url} onChange={updateAvatarUrl} />
           </div>
           <div>
             <label htmlFor="about">About</label>
-            <textarea onChange={updateAboutText} />
+            <textarea value={about} onChange={updateAboutText} />
           </div>
           <div className="button-container">
             <button>Save profile</button>
@@ -67,7 +74,7 @@ export const ProfileControlledInputs = () => {
       <div className="user-card">
         <img src={url} />
         <h1>
-          {name} {age === null ? "" : `(${age})`}
+          {name} {age === "" ? "" : `(${age})`}
         </h1>
         {/* {age !== null && <span>({age})</span>} */}
         <p>{about}</p>
