8f6a651bbe50aa1915dd049c7b563750c15a38fd
ixtk <ikhcamot@gmail.com>
2024-12-12 18:30:02 +0400

    Tweak styles

--- lesson-code/frontend/src/main.jsx
+++ lesson-code/frontend/src/main.jsx
@@ -1,11 +1,12 @@
-import { StrictMode } from "react"
-import { createRoot } from "react-dom/client"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 // import { MovieWatchlist } from "./movie-watchlist/App"
-import { ProfileControlledInputs } from "./forms/App"
+import { ProfileControlledInputs } from "./forms/App";
+import "./App.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <MovieWatchlist /> */}
     <ProfileControlledInputs />
   </StrictMode>
-)
+);
