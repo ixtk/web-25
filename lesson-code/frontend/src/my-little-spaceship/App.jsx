634632b3fd6b9facf60c0eaa92dbd4b91b23021b
ixtk <ikhcamot@gmail.com>
2025-01-23 12:56:36 +0400

    Add spaceship starter code

---
+++ lesson-code/frontend/src/my-little-spaceship/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css"
+
+export const Spaceship = () => {
+  const shapes = ["saucer", "rocket", "sphere", "pyramid"]
+  const materials = ["crystal", "plastic", "glass", "paper"]
+  const colors = ["red", "blue", "purple", "orange"]
+  const styles = ["illustration", "cartoonish", "pixelArt", "scifi"]
+  const backgroundElements = ["nebula", "asteroids", "planets", "stars"]
+
+  // classNames for container divs:
+  // name, color, shape, material, style, background
+
+  return (
+    <div>
+      <h1>Hello World 🌍</h1>
+    </div>
+  )
+}
