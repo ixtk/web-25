fa3b06a8cf874749c6708993ee9a3fab85de936b
ixtk <ikhcamot@gmail.com>
2025-01-26 15:40:25 +0400

    Add spaceship inputs

--- lesson-code/frontend/src/main.jsx
+++ lesson-code/frontend/src/main.jsx
@@ -2,14 +2,19 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 // import { MovieWatchlist } from "./movie-watchlist/App"
 // import { ProfileControlledInputs } from "./forms/AppWithOneState"
+// import { ProfileControlledInputs } from "./forms/App"
 // import { ProfileWithoutStates } from "./forms/AppWithoutStates"
-import { FormsPlayground } from "./forms-playground/App"
+// import { FormsPlayground } from "./forms-playground/App"
+// import { ProfileControlledInputs } from "./forms/App"
 import "./App.css"
+import { Spaceship } from "./my-little-spaceship/App"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <FormsPlayground />
+    {/* <ProfileControlledInputs /> */}
+    {/* <FormsPlayground /> */}
     {/* <MovieWatchlist /> */}
     {/* <ProfileWithoutStates /> */}
+    <Spaceship />
   </StrictMode>
 )
--- lesson-code/frontend/src/my-little-spaceship/App.jsx
+++ lesson-code/frontend/src/my-little-spaceship/App.jsx
@@ -12,7 +12,77 @@ export const Spaceship = () => {
 
   return (
     <div>
-      <h1>Hello World 🌍</h1>
+      <h1>My Little Spaceship</h1>
+      <form>
+        <div className="name">
+          <label htmlFor="name">Name</label>
+          <input id="name" type="text" />
+        </div>
+        <div className="shape">
+          <label>Shape</label>
+          <div>
+            {shapes.map((shape) => {
+              return (
+                <label key={shape}>
+                  <input type="radio" name="shape" />
+                  {shape}
+                </label>
+              )
+            })}
+          </div>
+        </div>
+        <div className="background">
+          <label>Background elements</label>
+          <div>
+            {backgroundElements.map((background) => {
+              return (
+                <label key={background}>
+                  <input type="checkbox" name={background} />
+                  {background}
+                </label>
+              )
+            })}
+          </div>
+        </div>
+        <div className="color">
+          <label htmlFor="">Color</label>
+          <div>
+            {colors.map((color) => {
+              return (
+                <button
+                  style={{ backgroundColor: color }}
+                  key={color}
+                  className="color"
+                ></button>
+              )
+            })}
+          </div>
+        </div>
+        <div className="material">
+          <label>Material</label>
+          <div>
+            {materials.map((material) => {
+              return (
+                <label key={material}>
+                  <input type="radio" name="material" />
+                  {material}
+                </label>
+              )
+            })}
+          </div>
+        </div>
+        <div className="style">
+          <label>Style</label>
+          <select name="style">
+            <option value="">---</option>
+            <option value="illustration">Illustration</option>
+            <option value="cartoonish">Cartoonish</option>
+            <option value="pixelArt">Pixel Art</option>
+            <option value="scifi">Sci-fi</option>
+          </select>
+        </div>
+        <button type="submit">Create!</button>
+      </form>
     </div>
   )
 }
