061ae5e04719dfa30c88ca3a8b3838623b5a847c
ixtk <ikhcamot@gmail.com>
2025-01-31 19:37:08 +0400

    Add hw-9 solution

--- homeworks/frontend/src/main.jsx
+++ homeworks/frontend/src/main.jsx
@@ -1,17 +1,19 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
 // import { BlogComponent } from "./hw-2-blog-component/App";
 // import { JobListingsConditionals } from "./hw-3-job-listings-conditionals/App"
 // import { FlightsTable } from "./hw-5-flights-table/App"
 // import { FigurineCreator } from "./hw-7-figurine-creator/App";
-import { OneWayChat } from "./hw-8-one-way-chat/App";
+// import { OneWayChat } from "./hw-8-one-way-chat/App";
+import { GameSettings } from "./hw-9-game-settings/App"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <BlogComponent /> */}
     {/* <FlightsTable /> */}
     {/* <JobListingsConditionals /> */}
-    <OneWayChat />
+    {/* <OneWayChat /> */}
     {/* <FigurineCreator /> */}
+    <GameSettings />
   </StrictMode>
-);
+)
