851910941e5662210c4dbbedc1ca1903ff113145
ixtk <ikhcamot@gmail.com>
2025-02-09 15:11:26 +0400

    Work on error handling

--- lesson-code/frontend/src/my-little-spaceship/App.jsx
+++ lesson-code/frontend/src/my-little-spaceship/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from "react"
-import "./App.css"
-import { useFormik } from "formik"
-import { object, string, array } from "yup"
+import { useState } from "react";
+import "./App.css";
+import { useFormik } from "formik";
+import { object, string, array } from "yup";
 
 export const Spaceship = () => {
-  const shapes = ["saucer", "rocket", "sphere", "pyramid"]
-  const materials = ["crystal", "plastic", "glass", "paper"]
-  const colors = ["red", "blue", "purple", "orange", "cornflowerblue"]
-  const styles = ["illustration", "cartoonish", "pixelArt", "scifi"]
-  const backgroundElements = ["nebula", "asteroids", "planets", "stars"]
+  const shapes = ["saucer", "rocket", "sphere", "pyramid"];
+  const materials = ["crystal", "plastic", "glass", "paper"];
+  const colors = ["red", "blue", "purple", "orange", "cornflowerblue"];
+  const styles = ["illustration", "cartoonish", "pixelArt", "scifi"];
+  const backgroundElements = ["nebula", "asteroids", "planets", "stars"];
 
-  const [resultUrl, setResultUrl] = useState("")
+  const [resultUrl, setResultUrl] = useState("");
 
   const formSchema = object({
     spaceshipName: string().min(4).required(),
@@ -18,8 +18,8 @@ export const Spaceship = () => {
     color: string().min(4).required(),
     style: string().min(4).required(),
     material: string().min(4).required(),
-    backgroundElements: array().of(string().min(3))
-  })
+    backgroundElements: array().of(string().min(3)),
+  });
 
   const spaceshipForm = useFormik({
     initialValues: {
@@ -28,30 +28,30 @@ export const Spaceship = () => {
       color: "",
       style: "",
       material: "",
-      backgroundElements: []
+      backgroundElements: [],
     },
     validationSchema: formSchema,
     onSubmit: async (data, formikHelpers) => {
-      console.log("Submitting form!")
-      console.log(data)
+      console.log("Submitting form!");
+      console.log(data);
 
       const response = await fetch("https://api.abcd.ge/spaceship", {
         method: "POST",
         headers: {
-          "content-type": "application/json"
+          "content-type": "application/json",
         },
-        body: JSON.stringify(data)
-      })
-      console.log(response)
+        body: JSON.stringify(data),
+      });
+      console.log(response);
 
-      const json = await response.json()
-      console.log(json)
-      setResultUrl(json.url)
-      formikHelpers.setSubmitting(false)
-    }
-  })
+      const json = await response.json();
+      console.log(json);
+      setResultUrl(json.url);
+      formikHelpers.setSubmitting(false);
+    },
+  });
 
-  // console.log("ERRORS:", spaceshipForm.errors)
+  console.log(spaceshipForm);
 
   // classNames for container divs:
   // name, color, shape, material, style, background
@@ -67,10 +67,13 @@ export const Spaceship = () => {
             type="text"
             name="spaceshipName"
             onChange={spaceshipForm.handleChange}
+            onBlur={spaceshipForm.handleBlur}
           />
-          <span style={{ color: "red" }}>
-            {spaceshipForm.errors.spaceshipName}
-          </span>
+          {spaceshipForm.touched.spaceshipName && (
+            <span style={{ color: "red" }}>
+              {spaceshipForm.errors.spaceshipName}
+            </span>
+          )}
         </div>
         <div className="shape">
           <label>Shape</label>
@@ -82,13 +85,17 @@ export const Spaceship = () => {
                     type="radio"
                     value={shape}
                     onChange={spaceshipForm.handleChange}
+                    onBlur={spaceshipForm.handleBlur}
                     name="shape"
                   />
                   {shape}
                 </label>
-              )
+              );
             })}
-            <span style={{ color: "red" }}>{spaceshipForm.errors.shape}</span>
+
+            {spaceshipForm.touched.shape && (
+              <span style={{ color: "red" }}>{spaceshipForm.errors.shape}</span>
+            )}
           </div>
         </div>
         <div className="background">
@@ -101,15 +108,18 @@ export const Spaceship = () => {
                     type="checkbox"
                     value={background}
                     name="backgroundElements"
+                    onBlur={spaceshipForm.handleBlur}
                     onChange={spaceshipForm.handleChange}
                   />
                   {background}
                 </label>
-              )
+              );
             })}
-            <span style={{ color: "red" }}>
-              {spaceshipForm.errors.backgroundElements}
-            </span>
+            {spaceshipForm.touched.backgroundElements && (
+              <span style={{ color: "red" }}>
+                {spaceshipForm.errors.backgroundElements}
+              </span>
+            )}
           </div>
         </div>
         <div className="color">
@@ -121,13 +131,16 @@ export const Spaceship = () => {
                   onClick={() => spaceshipForm.setFieldValue("color", col)}
                   type="button"
                   style={{ backgroundColor: col }}
+                  onBlur={spaceshipForm.handleBlur}
                   key={col}
                   className="color"
                 ></button>
-              )
+              );
             })}
           </div>
-          <span style={{ color: "red" }}>{spaceshipForm.errors.color}</span>
+          {spaceshipForm.touched.color && (
+            <span style={{ color: "red" }}>{spaceshipForm.errors.color}</span>
+          )}
         </div>
         <div className="material">
           <label>Material</label>
@@ -139,30 +152,41 @@ export const Spaceship = () => {
                     type="radio"
                     name="material"
                     value={material}
+                    onBlur={spaceshipForm.handleBlur}
                     onChange={spaceshipForm.handleChange}
                   />
                   {material}
                 </label>
-              )
+              );
             })}
 
-            <span style={{ color: "red" }}>
-              {spaceshipForm.errors.material}
-            </span>
+            {spaceshipForm.touched.material && (
+              <span style={{ color: "red" }}>
+                {spaceshipForm.errors.material}
+              </span>
+            )}
           </div>
         </div>
         <div className="style">
           <label>Style</label>
-          <select name="style" onChange={spaceshipForm.handleChange}>
+          <select
+            name="style"
+            onBlur={spaceshipForm.handleBlur}
+            onChange={spaceshipForm.handleChange}
+          >
             <option value="">---</option>
             <option value="illustration">Illustration</option>
             <option value="cartoonish">Cartoonish</option>
             <option value="pixelArt">Pixel Art</option>
             <option value="scifi">Sci-fi</option>
           </select>
-          <span style={{ color: "red" }}>{spaceshipForm.errors.style}</span>
+          {spaceshipForm.touched.style && (
+            <span style={{ color: "red" }}>{spaceshipForm.errors.style}</span>
+          )}
         </div>
-        <button type="submit">Create!</button>
+        <button disabled={spaceshipForm.isSubmitting} type="submit">
+          {spaceshipForm.isSubmitting ? "Loading..." : "Create!"}
+        </button>
         {spaceshipForm.isSubmitting ? (
           "Loading..."
         ) : (
@@ -170,5 +194,5 @@ export const Spaceship = () => {
         )}
       </form>
     </div>
-  )
-}
+  );
+};
