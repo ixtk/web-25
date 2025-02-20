2984f2f3b1f59648ff5a497cfc7a1dc3a749aed5
ixtk <ikhcamot@gmail.com>
2025-02-20 18:43:35 +0400

    Clear store file

--- lesson-code/frontend/src/store/App.jsx
+++ lesson-code/frontend/src/store/App.jsx
@@ -4,39 +4,10 @@ import { Product } from "./Product"
 
 export function Store() {
   const [productList, setProductList] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState("")
 
   const getProducts = async () => {
-    setIsLoading(true)
-    setError("")
-
-    /*
-    fetch("https://fakestoreapi.in/api/products")
-      .then((response) => {
-        return response.json();
-      })
-      .then((json) => {
-        setProductList(json.products);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        setError(error.message)
-        setIsLoading(false)
-        console.log("ERROR:", error.message);
-      });
-      */
-
-    try {
-      const response = await fetch("http://localhost:3000/products")
-      const json = await response.json()
-      setProductList(json)
-      setIsLoading(false)
-    } catch (error) {
-      setError(error.message)
-      setIsLoading(false)
-      console.log("ERROR:", error.message)
-    }
+    // todo...
   }
 
   const productListElements = productList.map((product) => {
@@ -56,11 +27,7 @@ export function Store() {
       <button onClick={getProducts}>Get all products</button>
       {error && <p style={{ color: "red" }}>{error}</p>}
 
-      {isLoading ? (
-        <div>Loading...</div>
-      ) : (
-        <div className="products-container">{productListElements}</div>
-      )}
+      <div className="products-container">{productListElements}</div>
     </div>
   )
 }
