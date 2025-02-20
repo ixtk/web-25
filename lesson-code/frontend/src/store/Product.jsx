fe51c2419d489af7a870b6a2ddfe29c767656e4b
ixtk <ikhcamot@gmail.com>
2025-02-20 18:43:35 +0400

    Clear store file

---
+++ lesson-code/frontend/src/store/Product.jsx
@@ -0,0 +1,9 @@
+export const Product = ({ title, imageUrl, price }) => {
+  return (
+    <div className="product">
+      <img src={imageUrl} alt="" />
+      <h2>{title}</h2>
+      <p>${price}</p>
+    </div>
+  );
+};
