dad83351a653884f4fd4e6da1576b463b7966978
ixtk <ikhcamot@gmail.com>
2025-03-01 10:15:56 +0400

    Add star wars fetch solution

--- lesson-code/frontend/src/main.jsx
+++ lesson-code/frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
-import "./App.css"
 
 // import { MovieWatchlist } from "./movie-watchlist/App"
 // import { ProfileControlledInputs } from "./forms/AppWithOneState"
@@ -11,9 +10,10 @@ import "./App.css"
 // import { ProfileControlledInputs } from "./forms/App"
 // import { Spaceship } from "./my-little-spaceship/App"
 // import { Spaceship as SpaceshipWithComponents } from "./my-little-spaceship/AppWithComponents"
-import { Store } from "./store/App"
-// import { StarWarsFetch } from "./star-wars-fetch/App"
-import { BrowserRouter } from "react-router";
+// import { Store } from "./store/App"
+import { StarWarsFetch } from "./star-wars-fetch/App"
+// import { BrowserRouter } from "react-router";
+import "./App.css"
 
 
 createRoot(document.getElementById("root")).render(
@@ -23,9 +23,9 @@ createRoot(document.getElementById("root")).render(
     {/* <MovieWatchlist /> */}
     {/* <ProfileWithoutStates /> */}
     {/* <SpaceshipWithComponents /> */}
-    <BrowserRouter>
+    {/* <BrowserRouter>
       <Store />
-    </BrowserRouter>
-    {/* <StarWarsFetch /> */}
+    </BrowserRouter> */}
+    <StarWarsFetch />
   </StrictMode>
 )
--- lesson-code/frontend/src/star-wars-fetch/App.jsx
+++ lesson-code/frontend/src/star-wars-fetch/App.jsx
@@ -1,10 +1,78 @@
+import { useState } from "react"
 import "./App.css"
 
 export function StarWarsFetch() {
+  const [characterName, setCharacterName] = useState("")
+  const [characterInfo, setCharacterInfo] = useState({
+    birthYear: "",
+    name: "",
+    planetPopulation: "",
+    planetTerrain: "",
+    planetName: ""
+  })
+  const [loading, setLoading] = useState(false)
+
+  const searchCharacterInfo = async (e) => {
+    e.preventDefault()
+    setLoading(true)
+
+    // TODO: error handling, clear input
+
+    const characterResponseObj = await fetch(
+      `https://swapi.dev/api/people/?search=${characterName}`
+    )
+    const characterJson = await characterResponseObj.json()
+
+    const planetResponseObj = await fetch(characterJson.results[0].homeworld)
+    const planetJson = await planetResponseObj.json()
+    console.log(planetJson)
+
+    setCharacterInfo({
+      birthYear: characterJson.results[0].birth_year,
+      name: characterJson.results[0].name,
+      planetName: planetJson.name,
+      planetPopulation: planetJson.population,
+      planetTerrain: planetJson.terrain
+    })
+    setLoading(false)
+  }
+
   return (
-    <div>
-      <h1>Star wars fetch</h1>
-      <input type="text" />
-    </div>
-  );
+    <>
+      <form onSubmit={searchCharacterInfo} style={{ maxWidth: "500px" }}>
+        <h1>Star wars fetch</h1>
+        <fieldset role="group">
+          <input
+            type="text"
+            placeholder="Search character"
+            onChange={(e) => setCharacterName(e.target.value)}
+          />
+          <button type="submit">Search</button>
+        </fieldset>
+      </form>
+      <div>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <ul>
+            {characterInfo.name && <li>Name: {characterInfo.name}</li>}
+            {characterInfo.birthYear && (
+              <li>Birth Year: {characterInfo.birthYear}</li>
+            )}
+            <ul>
+              {characterInfo.planetName && (
+                <li>Planet Name: {characterInfo.planetName}</li>
+              )}
+              {characterInfo.planetPopulation && (
+                <li>Planet Poupulation: {characterInfo.planetPopulation}</li>
+              )}
+              {characterInfo.planetTerrain && (
+                <li>Planet Terrain: {characterInfo.planetTerrain}</li>
+              )}
+            </ul>
+          </ul>
+        )}
+      </div>
+    </>
+  )
 }
