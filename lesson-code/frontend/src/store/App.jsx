71776d4bd27fbd0396112fd6175bdd8beefe442a
ixtk <ikhcamot@gmail.com>
2025-03-02 14:29:35 +0400

    Add another page

--- lesson-code/frontend/src/store/App.jsx
+++ lesson-code/frontend/src/store/App.jsx
@@ -2,11 +2,13 @@ import "./App.css";
 
 import { Routes, Route } from "react-router";
 import { HomePage } from "./HomePage";
+import { ProductPage } from "./ProductPage";
 
 export function Store() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
+      <Route path="/products/:id" element={<ProductPage />} />
     </Routes>
   );
 }
