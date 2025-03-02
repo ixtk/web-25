fa416f75e44ce1aaf5f9fc44d565420d6486f893
ixtk <ikhcamot@gmail.com>
2025-03-02 13:42:37 +0400

    Work on star wars fetch

--- lesson-code/frontend/src/star-wars-fetch/App.jsx
+++ lesson-code/frontend/src/star-wars-fetch/App.jsx
@@ -1,11 +1,57 @@
-import "./App.css"
+import { useState } from "react";
+import "./App.css";
 
 export const StarWarsFetch = () => {
+  const [characterName, setCharacterName] = useState("");
+  const [characterInfo, setCharacterInfo] = useState({
+    name: "",
+    birthYear: "",
+    planetPopulation: "",
+    planetTerrain: "",
+    planetName: ""
+  })
+  const [errorMsg, setErrorMsg] = useState("")
+
+  const searchCharacter = async () => {
+    const response = await fetch(`https://swapi.dev/api/people/?search=${characterName}`)
+    const characterJson = await response.json()
+
+    if (characterJson.results.length === 0) {
+      setErrorMsg("Character name is invalid")
+      return
+    }
+
+    const character = characterJson.results[0]
+
+    const planetResponse = await fetch(character.homeworld)
+    const planetJson = await planetResponse.json()
+
+    setCharacterInfo({
+      name: character.name,
+      birthYear: character.birth_year,
+      planetPopulation: planetJson.population,
+      planetTerrain: planetJson.terrain,
+      planetName: planetJson.name
+    })
+  }
+
   return (
     <div>
       <h1>Star wars fetch</h1>
-      <input type="text" />
-      <button>Search</button>
+      <input
+        value={characterName}
+        onChange={(e) => setCharacterName(e.target.value)}
+        type="text"
+      />
+      <span>{errorMsg}</span>
+      <ul>
+        <li>{characterInfo.name}</li>
+        <li>{characterInfo.birthYear}</li>
+        <li>{characterInfo.planetName}</li>
+        <li>{characterInfo.planetPopulation}</li>
+        <li>{characterInfo.planetTerrain}</li>
+      </ul>
+      <button onClick={searchCharacter}>Search</button>
     </div>
-  )
-}
+  );
+};
